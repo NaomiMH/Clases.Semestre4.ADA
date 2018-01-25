060dde01cd19651cbbbc51e2ca24337a147a24da
Naomi M. Honti <naomiperla.macias@hotmail.com>
2018-01-24 21:53:32 -0600

    Programa

--- CerosyUnos/main.cpp
+++ CerosyUnos/main.cpp
@@ -1,6 +1,6 @@
 /**
  * Naomi Macias Honti a01282098
- * 12 de enero 2017
+ * 12 de enero 2018
  * 16 min.
  * Se da una longitud de un string y el string, a partir de lo cual se tiene que regresar la cantidad de numeros que quedarian si se eligieran aleatoriamente dos numeros y dichos numeros fueran borrados siempre que resultaran ser un 0 y un 1.
  */
