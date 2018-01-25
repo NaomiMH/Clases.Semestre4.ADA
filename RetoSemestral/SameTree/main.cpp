54192c9d7662d1767e2c619fcc187ceee59d938e
Naomi M. Honti <naomiperla.macias@hotmail.com>
2018-01-24 21:47:17 -0600

    Programa

---
+++ RetoSemestral/SameTree/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p!=NULL && q!=NULL){
+            if(p->val==q->val){
+                if(isSameTree(p->right,q->right) && isSameTree(p->left,q->left))
+                    return true;
+            }
+        }
+        else if(p==NULL && q==NULL)
+            return true;
+        return false;
+    }
+};
