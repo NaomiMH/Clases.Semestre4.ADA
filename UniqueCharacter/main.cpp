ab8621c0f087878a653ae32776e573267777ab03
Naomi M. Honti <naomiperla.macias@hotmail.com>
2018-01-24 21:51:47 -0600

    Programa

--- UniqueCharacter/main.cpp
+++ UniqueCharacter/main.cpp
@@ -1,6 +1,6 @@
 /**
  * Naomi Macias Honti a01282098
- * 9 de enero 2017
+ * 9 de enero 2018
  * 30 min.
  * Recibe un texto de letras en mayusculas, las cuales estan repetidas y seguidas excepto por una, la cual es encontrada y mostrada en pantalla.
  */
